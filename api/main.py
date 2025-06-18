ad268851a96791ff3a5d712b2fc5feac11ca0aa7
whyash11 <106821947+whyash11@users.noreply.github.com>
2025-06-18 20:13:57 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from logic import answer_question
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def handle():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # AI Pipe / Render will set this
+    app.run(host="0.0.0.0", port=port, debug=True)
